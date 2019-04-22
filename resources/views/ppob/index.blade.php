c054fbf2c9572e0986ca1cca629e53cd8076134d
Aviq Baihaqy <aviq.baihaqy@gmail.com>
2019-04-22 13:04:39 +0700

    add PPOB sample

--- resources/views/ppob/index.blade.php
+++ resources/views/ppob/index.blade.php
@@ -29,32 +29,6 @@
                         <label for="installedPrinterName">Select an installed Printer:</label>
                         <select name="installedPrinterName" id="installedPrinterName" class="form-control"></select>
                     </div>
-                    <script type="text/javascript">
-                        //var wcppGetPrintersDelay_ms = 0;
-                        var wcppGetPrintersTimeout_ms = 10000; //10 sec
-                        var wcppGetPrintersTimeoutStep_ms = 500; //0.5 sec
-                        function wcpGetPrintersOnSuccess() {
-                            // Display client installed printers
-                            if (arguments[0].length > 0) {
-                                var p = arguments[0].split("|");
-                                var options = '';
-                                for (var i = 0; i < p.length; i++) {
-                                    options += '<option>' + p[i] + '</option>';
-                                }
-                                $('#installedPrinters').css('visibility', 'visible');
-                                $('#installedPrinterName').html(options);
-                                $('#installedPrinterName').focus();
-                                $('#loadPrinters').hide();
-                            } else {
-                                alert("No printers are installed in your system.");
-                            }
-                        }
-
-                        function wcpGetPrintersOnFailure() {
-                            // Do something if printers cannot be got from the client
-                            alert("No printers are installed in your system.");
-                        }
-                    </script>
                 </div>
                 <div class="col-md-4">
                     <hr/>
@@ -83,6 +57,33 @@
 @endsection
 
 @section('scripts')
+
+    <script type="text/javascript">
+        //var wcppGetPrintersDelay_ms = 0;
+        var wcppGetPrintersTimeout_ms = 10000; //10 sec
+        var wcppGetPrintersTimeoutStep_ms = 500; //0.5 sec
+        function wcpGetPrintersOnSuccess() {
+            // Display client installed printers
+            if (arguments[0].length > 0) {
+                var p = arguments[0].split("|");
+                var options = '';
+                for (var i = 0; i < p.length; i++) {
+                    options += '<option>' + p[i] + '</option>';
+                }
+                $('#installedPrinters').css('visibility', 'visible');
+                $('#installedPrinterName').html(options);
+                $('#installedPrinterName').focus();
+                $('#loadPrinters').hide();
+            } else {
+                alert("No printers are installed in your system.");
+            }
+        }
+
+        function wcpGetPrintersOnFailure() {
+            // Do something if printers cannot be got from the client
+            alert("No printers are installed in your system.");
+        }
+    </script>
 
     {!!
 
